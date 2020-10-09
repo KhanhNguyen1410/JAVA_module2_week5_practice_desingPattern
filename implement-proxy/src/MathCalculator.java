a03b61bee996c4e6f8e8b76d10ac9a33d5122b90
Khanh-Nobi-JR <69251285+Khanh-Nobi-JR@users.noreply.github.com>
2020-10-09 11:25:47 +0700

    add

---
+++ implement-proxy/src/MathCalculator.java
@@ -0,0 +1,23 @@
+public class MathCalculator implements Calculator {
+
+
+    @Override
+    public double add(double firstNum, double secondNum) {
+        return firstNum + secondNum;
+    }
+
+    @Override
+    public double sub(double firstNum, double secondNum) {
+        return firstNum - secondNum;
+    }
+
+    @Override
+    public double div(double firstNum, double secondNum) {
+        return firstNum / secondNum;
+    }
+
+    @Override
+    public double mul(double firstNum, double secondNum) {
+        return firstNum * secondNum;
+    }
+}
